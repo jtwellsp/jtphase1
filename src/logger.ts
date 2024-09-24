4703e45e6b85019966713e7c3bed4dcd9ae0ca6a
chloehumphrey <chloehumphrey@outlook.com>
2024-09-24 17:26:36 -0400

    Completed log levels, logs output to file

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,29 @@
 // logger.ts
 
 import { pino } from 'pino';
-import fs from 'fs';
+import path from 'path';
 
 import dotevn from 'dotenv';
 dotevn.config();
 
-
 // Log levels based on project requirements
+// 0: silent, 1: info, 2: debug, 3: error
 const logLevels = ['silent', 'info', 'debug', 'error']
 
+// Determine the log level from the environment variable, default to 'info'
 const level: string = process.env.LOG_LEVEL ? logLevels[parseInt(process.env.LOG_LEVEL)] : 'info';
 
-const logger = pino({
-  level: level,
-  transport: {
-    target: 'pino-pretty', // Pretty-print logs during development
-    options: {
-      colorize: true,       // Colorize logs for readability
-    },
+// Determine the log file path from the environment variable
+const logFilePath: string = process.env.LOG_FILE || '';  // Empty means no file, logs go to console
+
+const logger = pino(
+  {
+    level: level,
   },
-});
+  pino.destination({
+    dest: logFilePath, // Log file path
+    sync: false,       // Asynchronous logging for better performance
+  })
+);
 
 export default logger;
