a940b7a2c346f0f08fd885d7a488c6573b3ffdb0
WillKissel02 <wkissel@purdue.edu>
2024-09-26 10:14:57 -0400

    Fix net score calculation

--- src/models/scores/scorecard.ts
+++ src/models/scores/scorecard.ts
@@ -59,13 +59,13 @@ export class Scorecard {
     public calculateNetScore(): void {
         // Calculate net score based on Sarah's priorities
         // Responsive maintainers are her highest priority, she also wants low ramp up time
-        let rampUpWeight = 0.8
-        let correctnessWeight = 0.6
-        let busFactorWeight = 0.4
-        let responsiveMaintainerWeight = 0.9
-        let licenseWeight = 0.5
-        this.netScore = (this.rampUp * rampUpWeight + this.correctness * correctnessWeight + this.busFactor * busFactorWeight + this.responsiveMaintainer * responsiveMaintainerWeight + this.license * licenseWeight) / 5;
-        this.netScore_Latency =  parseFloat(((this.rampUp_Latency * rampUpWeight  + this.correctness_Latency * correctnessWeight + this.busFactor_Latency * busFactorWeight + this.responsiveMaintainer_Latency * responsiveMaintainerWeight + this.license_Latency * licenseWeight) / 5).toFixed(3));
+        let rampUpWeight = 0.2
+        let correctnessWeight = 0.3
+        let busFactorWeight = 0.1
+        let responsiveMaintainerWeight = 0.3
+        let licenseWeight = 0.1
+        this.netScore = parseFloat((this.rampUp * rampUpWeight + this.correctness * correctnessWeight + this.busFactor * busFactorWeight + this.responsiveMaintainer * responsiveMaintainerWeight + this.license * licenseWeight).toFixed(3));
+        this.netScore_Latency =  parseFloat((this.rampUp_Latency * rampUpWeight  + this.correctness_Latency * correctnessWeight + this.busFactor_Latency * busFactorWeight + this.responsiveMaintainer_Latency * responsiveMaintainerWeight + this.license_Latency * licenseWeight).toFixed(3));
     }
 
     // Convert all member variables to NJSON
